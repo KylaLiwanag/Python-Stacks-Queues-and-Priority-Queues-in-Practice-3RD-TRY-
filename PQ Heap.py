06b33271500e2beb1f52229a78e4113dc47b5ae1
KylaLiwanag <kylacamilleliwanag@gmail.com>
2022-12-17 04:21:52 +0800

    Building a Priority Queue Data Type

--- PQ Heap.py
+++ PQ Heap.py
@@ -7,3 +7,18 @@ heappush(fruits, "banana")
 
 fruits
 
+from heapq import heappop
+
+heappop(fruits)
+
+
+fruits
+
+
+person1 = ("John", "Brown", 42)
+person2 = ("John", "Doe", 42)
+person3 = ("John", "Doe", 24)
+
+person1 < person2
+
+person2 < person3
--- queues.py
+++ queues.py
@@ -22,3 +22,18 @@ class Queue:
 class Stack(Queue):
     def dequeue(self):
         return self._elements.pop()
+
+from collections import deque
+from heapq import heappop, heappush
+
+# ...
+
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (priority, value))
+
+    def dequeue(self):
+        return heappop(self._elements)

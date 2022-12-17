5bfa9fae5dd6ecb7f8b216218f8a9f33901d1011
KylaLiwanag <kylacamilleliwanag@gmail.com>
2022-12-17 23:58:57 +0800

    Handling Priority Queue updated

--- Handling PQ.py
+++ Handling PQ.py
@@ -1,4 +1,10 @@
+from queues import PriorityQueue
 from dataclasses import dataclass
+
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
 
 @dataclass
 class Message:
@@ -12,6 +18,7 @@ messages = PriorityQueue()
 
 messages.enqueue_with_priority(CRITICAL, wipers)
 messages.enqueue_with_priority(IMPORTANT, hazard_lights)
+
 messages.enqueue_with_priority(CRITICAL, Message("ABS engaged"))
 
 print(messages.dequeue())
